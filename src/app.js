ca160e102fac77182c4d598454d088f3149a39a8
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-09-20 17:19:37 +0530

    implement prettier eslint

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express from "express";
+import express from 'express';
 
 export const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send("Hello World!");
-})
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+});
